c453ec7a17728c696438a3cd062d64c2edec0eb1
ATB00ker <ajay39in@gmail.com>
2017-09-05 00:21:13 +0530

    [Security Enhancement] Migration to latest middleware.
    - Removed depreciated middleware (Jade & express) and updated to the latest versions (pub and express@4.X).
    - Made migrations as per the new middleware.
    - Changed secure login method.
    - Changed better README.md for Setup.
    - Introduced setup.js for setting up the databases required.

--- README.md
+++ README.md
@@ -1,11 +1,46 @@
-## The Amity Android Club
+# The Amity Android Club
 - This is the code repository for "The Amity Android Club", Official club of Amity School of Engineering and Technology.
 
-## How to install
-- Install Node.Js
-- Install mysql
-- Install mysql (npm install mysql), express (npm install express), Jade (npm isntall jade) inside directory.
-- Run Server (node app.js).
+# How to Setup
+### Install Node.Js
+- Linux(Terminal Commands for varies Distributions)
+'''
+https://nodejs.org/en/download/package-manager/
+'''
+- Windows(Download Link)
+'''
+https://nodejs.org/en/download/
+'''
 
-## Contributors
+### Install MySQL
+- Linux
+'''
+https://dev.mysql.com/doc/refman/5.7/en/linux-installation.html
+'''
+- Windows
+'''
+https://dev.mysql.com/doc/workbench/en/wb-installing-windows.html
+'''
+
+### Install Middleware for Node.Js (Inside "theAmityAndroidClub/")
+- body-parser@1.17.2
+- connect-flash@0.1.1
+- cookie-parser@1.4.3
+- express@4.15.4
+- express-session@1.15.5
+- mysql@2.14.1
+- node-datetime@2.0.0
+- pug@2.0.0-rc.3 extraneous
+
+### Set Database (Inside "theAmityAndroidClub/lib/")
+'''
+node setup.js
+'''
+
+### Run Server (Inside "theAmityAndroidClub/")
+'''
+node app.js
+'''
+
+# Contributors
 - [ATB00ker](https://github.com/ATB00ker)
--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 var express = require('express'),
-	path = require('path');
-var app = express.createServer();
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	http = require('http'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	mysql = require('mysql'),
+	flash=require("connect-flash");
+
+var app = express();
+var server = http.createServer(app);
+
+// Application Configure
+app.use(session({
+  secret: 'flash',
+  resave: true,
+  saveUninitialized: false,
+  cookie: { secure: true }
+}));
+app.use(checkAuth);
+app.set('view engine', 'pug');
+app.set('view options', { layout: false });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(flash());
 
 //Defining the public folder path.
 app.use(express.static(path.join(__dirname, '/views')));
 
 function checkAuth (req, res, next) { // don't serve following pages until user has logged in successfully.
-	if (req.url === '/admin' && (!req.session || !req.session.authenticated)) {
-		res.redirect('/login');
+	if (req.url === '/admin') {
+		var cookies = {};
+		req.headers && req.headers.cookie.split(';').forEach(function(cookie) {
+			var parts = cookie.match(/(.*?)=(.*)$/);
+			cookies[ parts[1].trim() ] = (parts[2] || '').trim();
+		});
+		if(database.query("SELECT COUNT(*) FROM loginSecret WHERE id = " + cookies['Holder'] + "and username = "+ cookies['Torch']) == 1)
+			res.redirect('/admin');
 		return;
 	}
 	next();
 }
-
-app.configure(function () {
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'flash' }));
-	app.use(express.bodyParser());
-	app.use(checkAuth);
-	app.use(app.router);
-	app.set('view engine', 'jade');
-	app.set('view options', { layout: false });
-});
 
 require('./lib/routes.js')(app);
 
